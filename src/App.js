a5daee50c4689b56ed933ee0f7f8c7eea878405b
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2022-09-21 01:39:20 +0600

    creating components, and returning HTML from them

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const number = 333;
+const vocalist = { name: "Dave Mustaine", band: "Megadeth" };
+const guitarist = { name: "Marty Friedman", band: "Megadeth" };
+
+const musicStyle = {
+  color: "#61dafb",
+  backgroundColor: "black",
+  padding: "10px 20px",
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>JSX</h3>
+        <div className="container">
+          <h6>Hello World</h6>
+        </div>
+        <div className="music">
+          <h6>Number1: {number}</h6>
+          <h6>Number2: {222 + number}</h6>
+          <h6 style={musicStyle}>
+            Vocalist: {vocalist.name}, Band: {vocalist.band}
+          </h6>
+          <h6
+            style={{
+              color: "#61dafb",
+              backgroundColor: "black",
+              padding: "10px 20px",
+            }}
+          >
+            Vocalist: {guitarist.name}, Band: {guitarist.band}
+          </h6>
+        </div>
       </header>
+      <h3>Components: Similar in Look, Different in Data</h3>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <h3>New Component</h3>
+      <Person2></Person2>
+      <Person2></Person2>
+      <Person2></Person2>
+      <Person2></Person2>
     </div>
   );
+}
+
+function Person(){
+  return(
+    <div className="person">
+      <h1> Alkaline Foster </h1>
+      <p> Learning React </p>
+    </div>
+  )
+}
+function Person2(){
+  return(
+    <div className="person">
+      <h1> Colkdaline Costfer </h1>
+      <p> Guarding Alkaline Foster </p>
+    </div>
+  )
 }
 
 export default App;
